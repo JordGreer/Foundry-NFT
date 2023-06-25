f54a4092c0d6905cbbfb8b7e0e879749bba9be77
Jordan <jordgreer@gmail.com>
2023-06-25 13:54:41 -0400

    small fix

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,19 +4,13 @@ pragma solidity ^0.8.0;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-
     uint256 private s_tokenCounter;
-
-
 
     constructor() ERC721("Dogie", "DOG") {}
 
-    function mintNft() public {
+    function mintNft() public {}
 
-    }
-
-    function tokenURI(uint256 tokenId) public ovverride view returns (string memory) {
-        return
-    }
-
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
 }
