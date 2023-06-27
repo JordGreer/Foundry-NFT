4c5649293dbc0f3ea7e4dc9763eabc0f23c61930
Jordan <jordgreer@gmail.com>
2023-06-27 18:32:55 -0400

    Finnish NFT minting logic

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,4 +1,5 @@
-//SPDX-Lcense-Identifier: MIT
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -6,11 +7,22 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
 
-    constructor() ERC721("Dogie", "DOG") {}
+    mapping(uint256 => string) private s_tokenIdToUri;
 
-    function mintNft() public {}
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
+    }
 }
