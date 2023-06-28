854ed9e32543d019a20f3088e7ca963cd70cfd89
Jordan <jordgreer@gmail.com>
2023-06-27 21:39:10 -0400

    Add interactions, deploy script, update ipfs

---
+++ script/Interactions.s.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant URI_TO_MINT =
+        "ipfs://QmeavoNbEZGTcMs3yNqU9eS8kYZasR6hRCmgs5ns6fDsUs";
+
+    function run() public {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(URI_TO_MINT);
+        vm.stopBroadcast();
+    }
+}
--- test/TestBasicNFT.t.sol
+++ test/TestBasicNFT.t.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {DeployBasicNft} from "script/DeployBasicNFT.s.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
 import {BasicNft} from "src/BasicNft.sol";
 import {Test} from "forge-std/Test.sol";
+import {MintBasicNft} from "script/Interactions.s.sol";
 
 contract BasicNFTTest is Test {
     DeployBasicNft public deployer;
@@ -12,7 +13,7 @@ contract BasicNFTTest is Test {
     address public USER = makeAddr("user");
 
     string public constant URI_TO_MINT =
-        "ipfs://QmWXWeAv837DbDFSaYJ7XQRXzGtUzJ6ysGWrhjexR6urCB";
+        "ipfs://QmeavoNbEZGTcMs3yNqU9eS8kYZasR6hRCmgs5ns6fDsUs";
 
     /* Think in terms of context of this file.  Its being run 
     by forge testing.  So This function will be run before 
@@ -63,4 +64,6 @@ contract BasicNFTTest is Test {
                 keccak256(abi.encodePacked(basicNft.tokenURI(0)))
         );
     }
+
+    //left video at 8:12:00
 }
